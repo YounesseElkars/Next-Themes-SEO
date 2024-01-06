f05e11e1659cd422f724494fba8a59b4f5df664b
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-06 14:21:57 +0100

    Feat : Under construction component

---
+++ src/app/underconstruction.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Underconstruction = () => {
+  return (
+    <div className="h-screen w-full bg-slate-900 ">
+      <div className="mx-auto w-1/2 pt-10 text-center text-3xl font-semibold capitalize text-slate-100">
+        under construction
+      </div>
+      <div className="mx-auto w-1/2 pt-10 text-center text-lg font-semibold text-slate-400  ">
+        This site is under construction. Exciting things coming soon!
+      </div>
+    </div>
+  );
+};
+
+export default Underconstruction;
