715d129200ca449e6623da791128d67599149372
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-06 18:09:36 +0100

    feat : bio , avat , divider , social-link components

---
+++ src/components/bio/avatar.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { TLink } from '@/types/general';
+
+type AvatarBioProps = {
+  link: TLink;
+  name: string;
+};
+
+const AvatarBio: FC<AvatarBioProps> = ({ link, name }) => {
+  return (
+    <Avatar>
+      <AvatarImage src={link} />
+      <AvatarFallback>
+        <div className="flex h-full w-full items-center justify-center text-[0.5rem]">{name}</div>
+      </AvatarFallback>
+    </Avatar>
+  );
+};
+
+export default AvatarBio;
---
+++ src/components/bio/divider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Divider = () => {
+  return (
+    <div className="verticalText gradiant-for-verical m-5 bg-slate-300 text-center text-xs tracking-[0.9rem] ">
+      SOCIAL
+    </div>
+  );
+};
+
+export default Divider;
