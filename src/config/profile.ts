143bc47ff2d03b624f3715160c3d4c28fd7b9a21
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-06 18:11:15 +0100

    feat : separte file for settings

---
+++ src/config/profile.ts
@@ -0,0 +1,12 @@
+import { ProfileSettingProps } from '@/types/general';
+
+export const profileSetting: ProfileSettingProps = {
+  myname: 'unes',
+  blogName: 'My Awesome Blog',
+  blogDescription: 'Hi 👋 My Name is Unes and im sharing react topics',
+  avatar: 'https://github.com/shadcn.png',
+  facebook: 'https://facebook.com',
+  instagram: 'https://instagram.com',
+  twiter: 'https://twitter.com',
+  linkedin: 'https://linkedin.com',
+};
