c846bf2ff64221a24a2ba2619842eaef68654803
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-06 20:23:05 +0100

    feat : adding postCard and postCards components

---
+++ src/components/post-cards/postCard.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import Link from 'next/link';
+
+type PostCardProps = { date: string; title: string; slug: string; subtitle: string };
+
+const PostCard: FC<PostCardProps> = ({ date, title, subtitle, slug }) => {
+  return (
+    <Link href={`/` + slug}>
+      <Card>
+        <CardHeader>
+          <CardDescription>{date}</CardDescription>
+          <CardTitle>{title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{subtitle}</p>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+};
+export default PostCard;
---
+++ src/components/post-cards/postCards.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PostCard from './postCard';
+import fs from 'fs';
+import matter from 'gray-matter';
+import { TPostCardDetails } from '@/types/general';
+
+export const getPostDetails = (): TPostCardDetails[] => {
+  const folder = './src/posts/';
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith('.md'));
+  const postDetails = markdownPosts.map((file) => {
+    const fileContents = fs.readFileSync(`./src/posts/${file}`, 'utf8');
+    const matterResult = matter(fileContents);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: file.replace('.md', ''),
+    };
+  });
+  return postDetails;
+};
+
+const PostCards = () => {
+  const postDetails: TPostCardDetails[] = getPostDetails();
+  return (
+    <div className="mt-4 grid h-auto w-full grid-cols-1 grid-rows-2  gap-4  rounded-md bg-blue-900/10 px-4 py-2  sm:grid-cols-2  sm:flex-row lg:grid-cols-3">
+      {postDetails.map((data) => (
+        <PostCard date={data.date} title={data.title} subtitle={data.subtitle} slug={data.slug} />
+      ))}
+    </div>
+  );
+};
+
+export default PostCards;
