ca0fc7a5c63dc1dc5dd8930b04e99007d25e413d
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-08 00:27:56 +0100

    style : removing space

--- src/components/full-post/FullPost.tsx
+++ src/components/full-post/FullPost.tsx
@@ -15,8 +15,8 @@ const bitter = Bitter({
 const FullPost: FC<TFullPostPage> = ({ title, subtitle, date, content, image, imageAlt }) => {
   return (
     <div className="flex flex-col  ">
-      <div className="my-12 text-6xl font-normal  ">{title}</div>
-      <div className="mb-6 text-base font-normal  ">{subtitle}</div>
+      <div className="my-12 text-6xl font-normal">{title}</div>
+      <div className="mb-6 text-base font-normal">{subtitle}</div>
       <div className={`${bitter.className} mb-2 text-xs `}>{date}</div>
       <Image src={image} height={1000} width={1000} className="w-full" alt={imageAlt} />
       <article className="  prose my-12 text-foreground  dark:prose-light">
