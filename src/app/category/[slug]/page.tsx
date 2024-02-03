f605914753e7309fd929a97da570192a3b947fc9
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-02-03 14:10:11 +0100

    adding generateMetadata for /category/[slug]

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,8 +1,18 @@
 import { ArticlesByCategory } from '@/components/posts/posts';
 import { getAllCategoriesCards } from '@/handlers/categoryHandler';
+import { Metadata, ResolvingMetadata } from 'next';
 import React from 'react';
 
 type params = { params: { slug: string } };
+
+export async function generateMetadata({ params }: params, parent: ResolvingMetadata): Promise<Metadata> {
+  const content = getAllCategoriesCards();
+  const { categoryName, categorySlug } = content[0];
+  return {
+    title: categoryName,
+    description: categorySlug,
+  };
+}
 
 export const generateStaticParams = async () => {
   const cats = getAllCategoriesCards();
