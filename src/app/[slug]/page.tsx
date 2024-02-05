865ee5245825e2b69803baf80a2c6854e73b24c7
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-02-05 14:15:35 +0100

    convert full article page to to minimal blog style

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -33,7 +33,7 @@ const page = ({ params }: params) => {
   const content = fullArticleHandler(slug);
 
   return (
-    <div className=" mt-4  h-auto w-full gap-4  rounded-md bg-blue-900/10 px-4 py-2  sm:grid-cols-2  sm:flex-row lg:grid-cols-3">
+    <div className="mt-4 h-auto w-full gap-4 px-4 py-2  sm:grid-cols-2  sm:flex-row lg:grid-cols-3">
       <FullArticle
         slug={content.slug}
         seoMetaDescription={content.seoMetaDescription}
