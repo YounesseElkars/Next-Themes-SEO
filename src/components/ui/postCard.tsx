3253cfe03ce9a2520e16518108299fabcda60ef7
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-02-05 14:11:01 +0100

    convert postcard style to minimal blog style

--- src/components/ui/postCard.tsx
+++ src/components/ui/postCard.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
 import { TFeaturedArticlesCards } from '@/types/general';
-// import Image from 'next/image';
 import ExportedImage from 'next-image-export-optimizer';
 
 const PostCard: FC<TFeaturedArticlesCards> = ({ title, subtitle, slug, image, imageAlt }) => {
   return (
     <Link aria-label={title + ' - Article'} href={`/` + slug}>
-      <div className="relative h-auto w-72 transform rounded-md transition-transform hover:scale-105 focus:border-blue-300 focus:ring ">
-        <ExportedImage src={image} height="100" width="100" alt={imageAlt} className="h-48 w-full rounded-md " />
-        <div className="bg absolute bottom-2 left-2 cursor-pointer select-none  rounded-md bg-blue-900/80 p-2 text-slate-300 hover:text-slate-50 ">
+      <div className="relative h-full w-full transform rounded-md transition-transform hover:scale-105 focus:border-blue-300 focus:ring ">
+        <div className="absolute h-full w-full bg-slate-950/10"></div>
+        <ExportedImage src={image} height="600" width="600" alt={imageAlt} className="h-full w-full object-cover" />
+        <div className="absolute bottom-4 left-4 cursor-pointer select-none text-white hover:text-slate-50">
           <div className="text-base">{title}</div>
-          <p className="text-xs">{subtitle}</p>
+          <p className="text-[0.5rem]">{subtitle}</p>
         </div>
       </div>
     </Link>
