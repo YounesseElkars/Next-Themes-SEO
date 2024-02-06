9c61bbf7483521e099c12b0c8df22956d62ad158
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-02-06 14:48:47 +0100

    new style : postcard comp.

--- src/components/ui/postCard.tsx
+++ src/components/ui/postCard.tsx
@@ -8,10 +8,10 @@ const PostCard: FC<TFeaturedArticlesCards> = ({ title, subtitle, slug, image, im
     <Link aria-label={title + ' - Article'} href={`/` + slug}>
       <div className="relative h-full w-full transform rounded-md transition-transform hover:scale-105 focus:border-blue-300 focus:ring ">
         <div className="absolute h-full w-full bg-slate-950/10"></div>
-        <ExportedImage src={image} height="600" width="600" alt={imageAlt} className="h-full w-full object-cover" />
-        <div className="absolute bottom-4 left-4 cursor-pointer select-none text-white hover:text-slate-50">
-          <div className="text-base">{title}</div>
-          <p className="text-[0.5rem]">{subtitle}</p>
+        <ExportedImage src={image} height="1000" width="1000" alt={imageAlt} className="h-full w-full object-cover" />
+        <div className="absolute bottom-0 left-0 w-full cursor-pointer select-none p-4 text-slate-50  ">
+          <div className="mb-3 text-xl underline decoration-primary decoration-1 underline-offset-4">{title}</div>
+          <p className="text-xs   ">{subtitle}</p>
         </div>
       </div>
     </Link>
