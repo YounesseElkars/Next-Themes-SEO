8e34a8b633fc4270ea106a5c41db57aef653af7a
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-02-07 23:02:51 +0100

    fix : removing unecessary types

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,22 +1,6 @@
 export type TLink = `http://${string}` | `https://${string}`;
-export type TImageLink = `./${string}` | `/${string}` | `http://${string}` | `https://${string}`;
 export type TLinkPath = `/${string}`;
-
-export type ProfileSettingProps = {
-  myname: string;
-  blogName: string;
-  blogDescription: string;
-  avatar: TLink;
-  facebook: TLink;
-  instagram: TLink;
-  twiter: TLink;
-  linkedin: TLink;
-  metaTitle: string;
-  metaDescription: string;
-  metaGraphImage: TImageLink;
-  metaKeywords: string[];
-  metadataBase: URL;
-};
+export type TImageLink = `./${string}` | `/${string}` | `http://${string}` | `https://${string}`;
 
 export type TFullPost = {
   title: string;
