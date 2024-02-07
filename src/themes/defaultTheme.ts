c261d642f24c26fb733fe1542591f775dd037855
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-02-07 23:02:36 +0100

    feat : theme swicther

---
+++ src/themes/defaultTheme.ts
@@ -0,0 +1,3 @@
+import * as Theme from './minimal';
+
+export default Theme;
---
+++ src/themes/minimal/index.ts
@@ -0,0 +1,23 @@
+import AboutTheAuthor from './About/AboutTheAuthor';
+import QuoteAuthor from './About/QuoteAuthor';
+import { FeaturedArticlesCards, ArticlesByCategory, FullArticle } from './Posts/Posts';
+import { ShowAllCategoriesCards } from './Categories/ShowAllCategoriesCards';
+import { Navbar } from './Navbar/Navbar';
+import { Container } from './Ui/Container';
+import { Footer } from './Footer/Footer';
+import { NotFound } from './Not-found/Not-found';
+import { Underconstruction } from './Underconstruction/Underconstruction';
+
+export {
+  Underconstruction,
+  NotFound,
+  Footer,
+  Container,
+  QuoteAuthor,
+  AboutTheAuthor,
+  FeaturedArticlesCards,
+  ArticlesByCategory,
+  FullArticle,
+  ShowAllCategoriesCards,
+  Navbar,
+};
