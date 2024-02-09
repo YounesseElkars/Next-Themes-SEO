24956692ec0595b5cc6f16a97f03faf86fe3f6e7
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-02-09 14:29:01 +0100

    adding second theme : posts comp.

--- src/themes/second/Posts/Posts.tsx
+++ src/themes/second/Posts/Posts.tsx
@@ -19,17 +19,16 @@ export const FeaturedArticlesCards = () => {
 
   return (
     <section className="mt-20">
-      <h2 className="mb-20 text-6xl underline decoration-primary decoration-1 underline-offset-8 ">
-        Featured Articles
-      </h2>
+      <h2 className="mb-5 text-2xl">Featured Articles</h2>
       <div className="grid auto-rows-[200px] grid-cols-2 gap-5 md:grid-cols-3 md:gap-8 lg:gap-14">
         {postDetails.slice(0, 6).map((_, i) => {
           return (
             <div
               key={i}
-              className={`row-span-1 ${i === 3 || i === 5 ? 'col-span-1    md:col-span-2' : ''}    ${
-                i === 2 ? ' row-span-1 md:row-span-2 ' : ''
-              } `}
+              className={`row-span-1 my-8 md:my-2
+              ${i === 3 || i === 5 ? 'col-span-1 md:col-span-2' : ''}
+              ${i === 2 ? ' row-span-1 md:row-span-2 ' : ''}
+              `}
             >
               <PostCard
                 key={i}
@@ -51,9 +50,7 @@ export const ArticlesByCategory = ({ slug }: { slug: string }) => {
   const articles = ShowArticlesCardsByCategory(slug);
   return (
     <section className="mt-20">
-      <h2 className="mb-20 break-words text-6xl underline decoration-primary decoration-1 underline-offset-8 ">
-        {slug}
-      </h2>
+      <h2 className="mb-5 text-2xl">{slug}</h2>
       <div className=" grid h-auto w-full grid-cols-1 gap-14 rounded-md px-4 py-2 sm:grid-cols-2 sm:flex-row lg:grid-cols-4 ">
         {articles.map((data, index) => (
           <PostCard
@@ -72,13 +69,13 @@ export const ArticlesByCategory = ({ slug }: { slug: string }) => {
 
 export const FullArticle: FC<TFullPost> = ({ title, subtitle, date, content, image, imageAlt }) => {
   return (
-    <div className="flex w-full flex-col">
+    <div className=" flex w-full flex-col">
       <div className="mb-2 mt-12 text-center text-6xl font-normal">{title}</div>
       <div className=" mb-2 text-center text-base font-normal ">{subtitle}</div>
       <div className={`${bitter.className} mb-12 text-center text-xs `}>{date}</div>
-      <ExportedImage src={image} height={1000} width={1000} className="w-full" alt={imageAlt} />
+      <ExportedImage src={image} height={1000} width={1000} className="  w-full" alt={imageAlt} />
 
-      <article className="prose my-12 w-full  max-w-7xl text-foreground dark:prose-light ">
+      <article className="prose mx-auto my-12 w-full max-w-7xl text-foreground dark:prose-light">
         <ReactMarkdown
           components={{
             img: ({ src, alt }) => <ExportedImage src={src as string} alt={alt as string} width={1200} height={200} />,
